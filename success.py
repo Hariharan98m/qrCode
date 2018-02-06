947084376070782375d17b83154bc8219a1fa31e
Hariharan98m <hariharan.m2016@vitstudent.ac.in>
2018-02-06 16:07:26 +0530

    Update success.py

--- success.py
+++ success.py
@@ -25,15 +25,22 @@ img_arr=[
 
     ]
 
-def mask_pixel:
-    for i in range(20):
-        for j in range(20):
-            if not ((i<=8 and j<=8) or (i<=8 and j>=13) or (i>=13 and j<=8) or ((i==10 or i==12) and j==6) or (i==6 and (j==10 or j==12)))
-                if not((i+j)mod3):
+def mask_pixel():
+    for i in range(21):
+        for j in range(21):
+            if not ((i<=8 and j<=8) or (i<=8 and j>=13) or (i>=13 and j<=8) or ((i==10 or i==12) and j==6) or (i==6 and (j==10 or j==12))):
+                if not((i+j)%3):
+                    print('(i,j)=',i,j)
                     if(img_arr[i][j]==0):
                         img_arr[i][j]=1
                     else:
                         img_arr[i][j]=0
+
+
+mask_pixel()
+
+print(img_arr)
+
 for i in range(21):
     for j in range(21):
         if(img_arr[i][j]==0):
@@ -46,4 +53,4 @@ pixels=img.load()
 for i in range(img.size[0]):
     for j in range(img.size[1]):
             pixels[i,j]=img_arr[i][j]
-img.save('Sout.png')
+img.save('C:\\Users\\HARIHARAN\\Desktop\\S1out.png')
